3376347d1de3e3beee7049694a11d2e1a38f66d1
krist <krist@LAPTOP-NBTNJTVJ>
2023-09-04 16:55:21 +0200

    123

---
+++ src/main/java/WEB/SR262021/Sr262021Application.java
@@ -0,0 +1,13 @@
+package WEB.SR262021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sr262021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sr262021Application.class, args);
+	}
+
+}
---
+++ src/test/java/WEB/SR262021/Sr262021ApplicationTests.java
@@ -0,0 +1,13 @@
+package WEB.SR262021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sr262021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
